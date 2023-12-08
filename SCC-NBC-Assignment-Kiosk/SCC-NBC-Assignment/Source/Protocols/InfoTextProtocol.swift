adb2962d3896abf773406e9d40ded7b031bdc8e7
joonBaek12 <joonbaek12@syuin.ac.kr>
2023-12-08 10:41:39 +0900

    [Add] #1 - InfoTextProtocol, Order, ProductDetail 추가

---
+++ SCC-NBC-Assignment-Kiosk/SCC-NBC-Assignment/Source/Protocols/InfoTextProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  InfoTextProtocol.swift
+//  SCC-NBC-Assignment
+//
+//  Created by Joon Baek on 2023/12/08.
+//
+
+import Foundation
+
+protocol InfoTextProtocol: Any {
+    var text: String? { get set }
+}
---
+++ SCC-NBC-Assignment-Kiosk/SCC-NBC-Assignment/Source/Views/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  SCC-NBC-Assignment
+//
+//  Created by Joon Baek on 2023/12/08.
+//
+
+import Foundation
